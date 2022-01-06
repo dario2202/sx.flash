eb6b6eec78b78dc36eb9cb1d823eaed7fbd9badb
zuelldar <49819851+dario2202@users.noreply.github.com>
2022-01-06 01:51:00 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,11 @@
 cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
 
 # create accounts
-cleos create account eosio flash.sx EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio basic EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio callback EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio eosio.token EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
-cleos create account eosio myaccount EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+cleos create account eosio flash.sx 0x10Ff5ED1CC38173815465A3ADe74A02A794920e7
+cleos create account eosio basic 0x10Ff5ED1CC38173815465A3ADe74A02A794920e7
+cleos create account eosio callback 0x10Ff5ED1CC38173815465A3ADe74A02A794920e7
+cleos create account eosio eosio.token 0x10Ff5ED1CC38173815465A3ADe74A02A794920e7
+cleos create account eosio myaccount 0x10Ff5ED1CC38173815465A3ADe74A02A794920e7
 
 # deploy
 cleos set contract flash.sx . flash.sx.wasm flash.sx.abi
